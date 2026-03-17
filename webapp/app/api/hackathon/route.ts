97447460d321f8e1fcf40c649101d7a88b2301b1
SANJOY <paulsanjoy2923@gmail.com>
2026-03-17 21:04:34 +0530

    schema updated -> hackathon table added

---
+++ webapp/app/api/hackathon/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const { name } = await req.json()
+
+        if (!name) {
+            return NextResponse.json(
+                { error: "Hackathon name is required" },
+                { status: 400 }
+            )
+        }
+
+        const hackathon = await prisma.hackathon.create({
+            data: { name }
+        })
+
+        return NextResponse.json(
+            {
+                success: true,
+                hackathonId: hackathon.id,
+            },
+            { status: 201 }
+        )
+
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        )
+    }
+}
